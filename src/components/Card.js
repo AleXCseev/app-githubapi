788c63ade5db157a496a64e86831c7144997fa0d
Alekseev Konstantin <alexcseev1990@gmail.com>
2020-05-27 22:25:39 +0300

    add <Search> and <Card>

---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <img src={""} alt={""} className="card-img-top"></img>
+      <div className="card-body">
+        <h5 className="card-title">React JS</h5>
+        <Link to={"/profile/" + "react"} className="btn btn-primary">
+          Открыть
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import Search from "../components/Search";
+import Card from "../components/Card";
 
 const Home = () => {
+  const cards = new Array(15).fill("").map((_, i) => i);
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <React.Fragment>
+      <Search></Search>
+      <div className="row">
+        {cards.map((card) => {
+          return (
+            <div className="col-sm-4 mb-5" key={card}>
+              <Card></Card>
+            </div>
+          );
+        })}
+      </div>
+    </React.Fragment>
   );
-}
+};
 
 export default Home;
